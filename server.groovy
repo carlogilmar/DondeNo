554fcb8985acf169e09fe967753ad76589dfe0e1
carlogilmar <carlogilmar12@gmail.com>
2017-11-20 14:19:32 -0600

    Adding routes for show every case!

--- server.groovy
+++ server.groovy
@@ -8,6 +8,17 @@ def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 router.route().handler(BodyHandler.create())
 
+/*
+assault
+violation
+physicalViolence
+theftOfAutoParts
+extortion
+weaponInjury
+robberyWithViolence
+vehicleTheft
+*/
+
 router.get("/statics").handler({ routingContext ->
 	vertx.eventBus().send("com.carlogilmar.get.total.registers", "Total registers"){ reply ->
 		if(reply.succeeded()){
@@ -29,8 +40,85 @@ router.post("/newRegister").handler { routingContext ->
 	.end("Reporte Creado!")
 }
 
+router.get("/vehicleTheft").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"vehicleTheft"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
+router.get("/robberyWithViolence").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"robberyWithViolence"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
+router.get("/weaponInjury").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"weaponInjury"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
+router.get("/extortion").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"extortion"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
+router.get("/theftOfAutoParts").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"theftOfAutoParts"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
+router.get("/physicalViolence").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"physicalViolence"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
 router.get("/assault").handler({ routingContext ->
 	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"assault"]){ reply ->
+		if(reply.succeeded()){
+			routingContext.response()
+			.setStatusCode(200)
+			.putHeader("content-type", "application/json; charset=utf-8")
+			.end( Json.encodePrettily(reply.result().body()) )
+		}
+	}
+})
+
+router.get("/violation").handler({ routingContext ->
+	vertx.eventBus().send("com.carlogilmar.get.specific.registers", [type:"violation"]){ reply ->
 		if(reply.succeeded()){
 			routingContext.response()
 			.setStatusCode(200)
